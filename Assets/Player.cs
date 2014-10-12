2a7ade75cb3ee04e29513bc42467947365e64488
Gareth Williams <gareth@garethw.net>
2014-10-12 22:22:55 +0100

    Messing with destruction

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 
     public Map Map;
 
+    public Camera Camera;
+
 	// Use this for initialization
 	void Start () {
 	    
@@ -18,5 +20,23 @@ public class Player : MonoBehaviour
 
         if(Map.State!=MapState.Loaded) GetComponent<CharacterMotor>().enabled = false;
         else GetComponent<CharacterMotor>().enabled = true;
+
+	    if (Input.GetMouseButtonUp(0))
+	    {
+	        Ray desRay = Camera.ScreenPointToRay(new Vector3(Screen.width/2f, Screen.height/2f, 0f));
+
+            Debug.DrawRay(desRay.origin,desRay.direction * 10f,Color.yellow,0.1f);
+
+            
+
+	        for (float d = 0; d < 10f; d += 0.1f)
+	        {
+	            if (Map.GetVoxel(desRay.GetPoint(d)).Active)
+	            {
+	                Map.SetVoxel(desRay.GetPoint(d), false);
+	                break;
+	            }
+	        }
+	    }
 	}
 }
